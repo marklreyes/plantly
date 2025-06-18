443442167893e8ea8b88e66d2fec1cb4201fdb2b
Mark Reyes <mark.l.reyes@gmail.com>
2025-06-18 16:33:35 -0700

    Create root layout file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,19 @@
-import { Tabs } from "expo-router";
-import Entypo from "@expo/vector-icons/Entypo";
-import Feather from "@expo/vector-icons/Feather";
-import { theme } from "@/theme";
-
+import { Stack } from "expo-router";
 export default function Layout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorGreen }}>
-      <Tabs.Screen
-        name="index"
+    <Stack>
+      <Stack.Screen
+        name="(tabs)"
         options={{
-          title: "Home",
-          tabBarShowLabel: false,
-          tabBarIcon: ({ size, color }) => (
-            <Entypo name="leaf" size={size} color={color} />
-          ),
+          headerShown: false,
         }}
       />
-      <Tabs.Screen
-        name="profile"
+      <Stack.Screen
+        name="onboarding"
         options={{
-          title: "Profile",
-          tabBarShowLabel: false,
-          tabBarIcon: ({ size, color }) => (
-            <Feather name="user" size={size} color={color} />
-          ),
+          headerShown: false,
         }}
       />
-    </Tabs>
+    </Stack>
   );
 }
